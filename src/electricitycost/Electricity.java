01ec881d5497b3f006b6376db2e52d84554cf02c
Hiem-Sreynit <hiemsreynit282@gmail.com>
2026-01-28 13:21:36 +0700

    finalization

--- src/electricitycost/Electricity.java
+++ src/electricitycost/Electricity.java
@@ -1,4 +1,28 @@
 package electricitycost;
 
+import java.util.Scanner;
+
 public class Electricity {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int electricityUsage;
+        float electricityCost;
+
+        System.out.println("=".repeat(40));
+        System.out.println("ELECTRICITY COST PER MONTH");
+        System.out.println("=".repeat(40));
+
+        System.out.print("Enter your monthly electricity usage: ");
+        electricityUsage = input.nextInt();
+
+        electricityCost = (electricityUsage > 0 && electricityUsage <= 50) ? 1500.0f : 2000.0f;
+        float total = electricityUsage * electricityCost;
+
+        System.out.println("ELECTRICITY USAGE PER MONTH: " + electricityUsage);
+        System.out.println("ELECTRICITY COST PER MONTH: " + total + "KHR");
+        System.out.println("=".repeat(40));
+
+    }
 }
+
--- src/empregistrationsystem/EmpRegistration.java
+++ src/empregistrationsystem/EmpRegistration.java
@@ -1,4 +1,73 @@
 package empregistrationsystem;
 
-public class EmpRegistration {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+enum EmployeeType {
+    FULL_TIME,
+    PART_TIME
 }
+
+public class EmpRegistration {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int id;
+        String name;
+        String email;
+        int age;
+        float oriSalary;
+        String empType;
+
+        System.out.println("+".repeat(60));
+        System.out.println("EMPLOYEE REGISTRATION SYSTEM");
+        System.out.println("+".repeat(60));
+
+        System.out.print("Enter ID: ");
+        id = input.nextInt();
+        input.nextLine();
+        System.out.print("Enter NAME: ");
+        name = input.nextLine();
+        System.out.print("Enter EMAIL: ");
+        email = input.nextLine();
+        System.out.print("Enter AGE: ");
+        age = input.nextInt();
+        System.out.print("Enter ORIGINAL SALARY: ");
+        oriSalary = input.nextFloat();
+        input.nextLine();
+        System.out.print("Enter EMPLOYEE TYPE (FULL_TIME, PART_TIME): ");
+        empType = input.nextLine();
+
+        String idValidation = (id <= 0) ? "INVALID ID!" : "ID: " + id;
+        String ageValidation = (age >= 18 && age <= 65) ? "AGE: " + age : "AGE: 0";
+        String oriSalaryMessage = (oriSalary < 0) ? "ORIGINAL SALARY: 0" : "ORIGINAL SALARY: " + oriSalary;
+
+        Pattern p = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
+        Matcher m = p.matcher(email);
+        String emailValidation = (m.matches()) ? "EMAIL: " + email : "INVALID EMAIL!";
+
+        Pattern p1 = Pattern.compile("^[a-zA-Z.]{3,30}$");
+        Matcher m1 = p1.matcher(name);
+        String nameValidation = (m1.matches()) ? "NAME: " + name : "INVALID NAME!";
+
+        Pattern p2 = Pattern.compile("\\bFULL_TIME|PART_TIME\\b");
+        Matcher m2 = p2.matcher(empType);
+        String empTypeValidation = (m2.matches()) ? "EMPLOYEE'S TYPE: " + empType : "EMPLOYEE'S TYPE: Null";
+
+        String finalSalary = (EmployeeType.FULL_TIME.equals(empTypeValidation)) ? "ORIGINAL SALARY: " + oriSalary : "FINAL SALARY: " + oriSalary * 0.6;
+
+        System.out.println("=".repeat(50));
+        System.out.println("EMPLOYEE INFORMATION");
+        System.out.println("=".repeat(50));
+        System.out.println(idValidation);
+        System.out.println(nameValidation);
+        System.out.println(ageValidation);
+        System.out.println(empTypeValidation);
+        System.out.println(oriSalaryMessage);
+        System.out.println(finalSalary);
+        System.out.println(emailValidation);
+    }
+}
+
+
